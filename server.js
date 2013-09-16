9e302ca2d9b3080a17892b4c2e77ea41ebb12e4a
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-09-15 20:38:40 -0600

    Some data units!

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var app = express();
 
-var app = express()
-    .use(express.static(path.join(__dirname, '/static')))
+app.get('/data/store/:filename/:data', function(req, res) {
+    fs.appendFile('data/' + req.params.filename + '.cvs', req.params.data + '\n', function (err) {
+        if (err) {
+            console.log(req.params.data + '\n ');
+        }
+        res.end();
+    });
+});
+
+app.use(express.static(path.join(__dirname, '/static')))
     .listen('3000');
