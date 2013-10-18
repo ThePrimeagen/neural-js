aa0fb1daa3925e6b3029222555784aa7d5f5ec1e
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-18 14:37:39 -0600

    yes

---
+++ experiments.sh
@@ -0,0 +1,23 @@
+node ./lib/hand-writer.js MLP opt-digits 2 1 40
+node ./lib/hand-writer.js MLP opt-digits 2 1 50
+node ./lib/hand-writer.js MLP opt-digits 2 1 60
+node ./lib/hand-writer.js MLP opt-digits 2 1 70
+node ./lib/hand-writer.js MLP opt-digits 2 2 40
+node ./lib/hand-writer.js MLP opt-digits 2 2 50
+node ./lib/hand-writer.js MLP opt-digits 2 2 60
+node ./lib/hand-writer.js MLP opt-digits 2 2 70
+node ./lib/hand-writer.js MLP opt-digits 2 3 40
+node ./lib/hand-writer.js MLP opt-digits 2 3 50
+node ./lib/hand-writer.js MLP opt-digits 2 3 60
+node ./lib/hand-writer.js MLP opt-digits 2 3 70
+node ./lib/hand-writer.js ANFIS opt-digits 2 2
+node ./lib/hand-writer.js ANFIS opt-digits 2 3
+node ./lib/hand-writer.js ANFIS opt-digits 2 4
+node ./lib/hand-writer.js ANFIS opt-digits 2 5
+node ./lib/hand-writer.js ANFIS opt-digits 2 6
+node ./lib/hand-writer.js RBF opt-digits 2 30
+node ./lib/hand-writer.js RBF opt-digits 2 40
+node ./lib/hand-writer.js RBF opt-digits 2 50
+node ./lib/hand-writer.js RBF opt-digits 2 60
+node ./lib/hand-writer.js RBF opt-digits 2 70
+node ./lib/hand-writer.js RBF opt-digits 2 80
