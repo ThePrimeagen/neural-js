ce66ff12dc60d821b921c2692493b4b84d1f429b
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-19 11:41:36 -0600

    Latest data

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,21 @@
+node ./lib/hand-writer.js RBF pen-digits 2 3
+node ./lib/hand-writer.js RBF pen-digits 2 4
+node ./lib/hand-writer.js RBF pen-digits 2 5
+node ./lib/hand-writer.js RBF pen-digits 2 6
+node ./lib/hand-writer.js RBF pen-digits 2 7
+node ./lib/hand-writer.js RBF pen-digits 2 8
+node ./lib/hand-writer.js RBF opt-digits 2 3
+node ./lib/hand-writer.js RBF opt-digits 2 4
+node ./lib/hand-writer.js RBF opt-digits 2 5
+node ./lib/hand-writer.js RBF opt-digits 2 6
+node ./lib/hand-writer.js RBF opt-digits 2 7
+node ./lib/hand-writer.js RBF opt-digits 2 8
+node ./lib/hand-writer.js RBF letter-rec 2 3
+node ./lib/hand-writer.js RBF letter-rec 2 4
+node ./lib/hand-writer.js RBF letter-rec 2 5
+node ./lib/hand-writer.js RBF letter-rec 2 6
+node ./lib/hand-writer.js RBF letter-rec 2 7
+node ./lib/hand-writer.js RBF letter-rec 2 8
 node ./lib/hand-writer.js MLP letter-rec 2 1 40
 node ./lib/hand-writer.js MLP letter-rec 2 1 50
 node ./lib/hand-writer.js MLP letter-rec 2 2 40
@@ -8,12 +26,6 @@ node ./lib/hand-writer.js MLP letter-rec 2 3 40
 node ./lib/hand-writer.js MLP letter-rec 2 3 50
 node ./lib/hand-writer.js MLP letter-rec 2 3 60
 node ./lib/hand-writer.js MLP letter-rec 2 3 70
-node ./lib/hand-writer.js RBF letter-rec 2 30
-node ./lib/hand-writer.js RBF letter-rec 2 40
-node ./lib/hand-writer.js RBF letter-rec 2 50
-node ./lib/hand-writer.js RBF letter-rec 2 60
-node ./lib/hand-writer.js RBF letter-rec 2 70
-node ./lib/hand-writer.js RBF letter-rec 2 80
 node ./lib/hand-writer.js ANFIS pen-digits 2 2
 node ./lib/hand-writer.js ANFIS pen-digits 2 3
 node ./lib/hand-writer.js ANFIS pen-digits 2 4
