0c683a9a2292a7d5c5f7193e4cbb4e5826b18e6f
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-19 15:40:00 -0600

    Experiments upgrade.

---
+++ experiments.sh
@@ -0,0 +1,40 @@
+node ./lib/hand-writer.js RBF letter-rec 2 &
+node ./lib/hand-writer.js RBF letter-rec 4 &
+node ./lib/hand-writer.js RBF letter-rec 6 &
+node ./lib/hand-writer.js RBF letter-rec 8 &
+node ./lib/hand-writer.js RBF letter-rec 10 &
+node ./lib/hand-writer.js MLP letter-rec 1 20 &
+node ./lib/hand-writer.js MLP letter-rec 1 40 &
+node ./lib/hand-writer.js MLP letter-rec 1 60 &
+node ./lib/hand-writer.js MLP letter-rec 1 80 &
+node ./lib/hand-writer.js MLP letter-rec 1 100 &
+node ./lib/hand-writer.js MLP letter-rec 2 20 &
+node ./lib/hand-writer.js MLP letter-rec 2 40 &
+node ./lib/hand-writer.js MLP letter-rec 2 60 &
+node ./lib/hand-writer.js MLP letter-rec 2 80 &
+node ./lib/hand-writer.js MLP letter-rec 2 100 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 &
+node ./lib/hand-writer.js ANFIS letter-rec 3 &
+node ./lib/hand-writer.js ANFIS letter-rec 4 &
+node ./lib/hand-writer.js ANFIS letter-rec 5 &
+node ./lib/hand-writer.js ANFIS letter-rec 6 &
+node ./lib/hand-writer.js RBF opt-digits 2 &
+node ./lib/hand-writer.js RBF opt-digits 4 &
+node ./lib/hand-writer.js RBF opt-digits 6 &
+node ./lib/hand-writer.js RBF opt-digits 8 &
+node ./lib/hand-writer.js RBF opt-digits 10 &
+node ./lib/hand-writer.js MLP opt-digits 1 20 &
+node ./lib/hand-writer.js MLP opt-digits 1 40 &
+node ./lib/hand-writer.js MLP opt-digits 1 60 &
+node ./lib/hand-writer.js MLP opt-digits 1 80 &
+node ./lib/hand-writer.js MLP opt-digits 1 100 &
+node ./lib/hand-writer.js MLP opt-digits 2 20 &
+node ./lib/hand-writer.js MLP opt-digits 2 40 &
+node ./lib/hand-writer.js MLP opt-digits 2 60 &
+node ./lib/hand-writer.js MLP opt-digits 2 80 &
+node ./lib/hand-writer.js MLP opt-digits 2 100 &
+node ./lib/hand-writer.js ANFIS opt-digits 2 &
+node ./lib/hand-writer.js ANFIS opt-digits 3 &
+node ./lib/hand-writer.js ANFIS opt-digits 4 &
+node ./lib/hand-writer.js ANFIS opt-digits 5 &
+node ./lib/hand-writer.js ANFIS opt-digits 6 
