44a86fa546e0cd2b32ca6af364c210eeea0e2c4d
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-19 14:14:18 -0600

    More data!

--- experiments.sh
+++ experiments.sh
@@ -1,31 +1,21 @@
-node ./lib/hand-writer.js RBF letter-rec 2 3
-node ./lib/hand-writer.js RBF letter-rec 2 4
-node ./lib/hand-writer.js RBF letter-rec 2 5
-node ./lib/hand-writer.js RBF letter-rec 2 6
-node ./lib/hand-writer.js RBF letter-rec 2 7
-node ./lib/hand-writer.js RBF letter-rec 2 8
-node ./lib/hand-writer.js MLP letter-rec 2 1 40
-node ./lib/hand-writer.js MLP letter-rec 2 1 50
-node ./lib/hand-writer.js MLP letter-rec 2 2 40
-node ./lib/hand-writer.js MLP letter-rec 2 2 50
-node ./lib/hand-writer.js MLP letter-rec 2 2 60
-node ./lib/hand-writer.js MLP letter-rec 2 2 70
-node ./lib/hand-writer.js MLP letter-rec 2 3 40
-node ./lib/hand-writer.js MLP letter-rec 2 3 50
-node ./lib/hand-writer.js MLP letter-rec 2 3 60
-node ./lib/hand-writer.js MLP letter-rec 2 3 70
-node ./lib/hand-writer.js ANFIS pen-digits 2 2
-node ./lib/hand-writer.js ANFIS pen-digits 2 3
-node ./lib/hand-writer.js ANFIS pen-digits 2 4
-node ./lib/hand-writer.js ANFIS pen-digits 2 5
-node ./lib/hand-writer.js ANFIS pen-digits 2 6
-node ./lib/hand-writer.js ANFIS letter-rec 2 6
-node ./lib/hand-writer.js ANFIS letter-rec 2 2
-node ./lib/hand-writer.js ANFIS letter-rec 2 3
-node ./lib/hand-writer.js ANFIS letter-rec 2 4
-node ./lib/hand-writer.js ANFIS letter-rec 2 5
-node ./lib/hand-writer.js ANFIS opt-digits 2 2
-node ./lib/hand-writer.js ANFIS opt-digits 2 3
-node ./lib/hand-writer.js ANFIS opt-digits 2 4
-node ./lib/hand-writer.js ANFIS opt-digits 2 5
+node ./lib/hand-writer.js MLP letter-rec 2 2 60 &
+node ./lib/hand-writer.js MLP letter-rec 2 2 70 &
+node ./lib/hand-writer.js MLP letter-rec 2 3 40 &
+node ./lib/hand-writer.js MLP letter-rec 2 3 50 &
+node ./lib/hand-writer.js MLP letter-rec 2 3 60 &
+node ./lib/hand-writer.js MLP letter-rec 2 3 70 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 2 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 3 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 4 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 5 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 6 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 6 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 2 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 3 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 4 &
+node ./lib/hand-writer.js ANFIS letter-rec 2 5 &
+node ./lib/hand-writer.js ANFIS opt-digits 2 2 &
+node ./lib/hand-writer.js ANFIS opt-digits 2 3 &
+node ./lib/hand-writer.js ANFIS opt-digits 2 4 &
+node ./lib/hand-writer.js ANFIS opt-digits 2 5 &
 node ./lib/hand-writer.js ANFIS opt-digits 2 6
