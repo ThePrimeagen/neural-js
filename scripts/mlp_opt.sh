ac80215f07b7f5670350bda222b51e1ba6703a46
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-19 20:49:00 -0600

    Removed some stuff.

---
+++ scripts/mlp_opt.sh
@@ -0,0 +1,10 @@
+node ./lib/hand-writer.js MLP opt-digits 1 20 &
+node ./lib/hand-writer.js MLP opt-digits 1 40 &
+node ./lib/hand-writer.js MLP opt-digits 1 60 &
+node ./lib/hand-writer.js MLP opt-digits 1 80 &
+node ./lib/hand-writer.js MLP opt-digits 1 100
+node ./lib/hand-writer.js MLP opt-digits 2 20 &
+node ./lib/hand-writer.js MLP opt-digits 2 40 &
+node ./lib/hand-writer.js MLP opt-digits 2 60 &
+node ./lib/hand-writer.js MLP opt-digits 2 80 &
+node ./lib/hand-writer.js MLP opt-digits 2 100 &
---
+++ scripts/rbf_all.sh
@@ -0,0 +1,10 @@
+node ./lib/hand-writer.js RBF letter-rec 2 &
+node ./lib/hand-writer.js RBF letter-rec 4 &
+node ./lib/hand-writer.js RBF letter-rec 6 &
+node ./lib/hand-writer.js RBF letter-rec 8 &
+node ./lib/hand-writer.js RBF letter-rec 10 
+node ./lib/hand-writer.js RBF opt-digits 2 &
+node ./lib/hand-writer.js RBF opt-digits 4 &
+node ./lib/hand-writer.js RBF opt-digits 6 &
+node ./lib/hand-writer.js RBF opt-digits 8 &
+node ./lib/hand-writer.js RBF opt-digits 10
