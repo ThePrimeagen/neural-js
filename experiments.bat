1fd1ebb98cb0cd920d40cc94350771c8b3326581
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-19 20:42:07 -0600

    Removed so called backprop.

---
+++ experiments.bat
@@ -0,0 +1,40 @@
+node ./lib/hand-writer.js RBF pen-digits 2 &
+node ./lib/hand-writer.js RBF pen-digits 4 &
+node ./lib/hand-writer.js RBF pen-digits 6 &
+node ./lib/hand-writer.js RBF pen-digits 8 &
+node ./lib/hand-writer.js RBF pen-digits 10 &
+node ./lib/hand-writer.js MLP pen-digits 1 20 &
+node ./lib/hand-writer.js MLP pen-digits 1 40 &
+node ./lib/hand-writer.js MLP pen-digits 1 60 &
+node ./lib/hand-writer.js MLP pen-digits 1 80 &
+node ./lib/hand-writer.js MLP pen-digits 1 100 &
+node ./lib/hand-writer.js MLP pen-digits 2 20 &
+node ./lib/hand-writer.js MLP pen-digits 2 40 &
+node ./lib/hand-writer.js MLP pen-digits 2 60 &
+node ./lib/hand-writer.js MLP pen-digits 2 80 &
+node ./lib/hand-writer.js MLP pen-digits 2 100 &
+node ./lib/hand-writer.js ANFIS pen-digits 2 &
+node ./lib/hand-writer.js ANFIS pen-digits 3 &
+node ./lib/hand-writer.js ANFIS pen-digits 4 &
+node ./lib/hand-writer.js ANFIS pen-digits 5 &
+node ./lib/hand-writer.js ANFIS pen-digits 6 &
+node ./lib/hand-writer.js RBF semeion-digits 2 &
+node ./lib/hand-writer.js RBF semeion-digits 4 &
+node ./lib/hand-writer.js RBF semeion-digits 6 &
+node ./lib/hand-writer.js RBF semeion-digits 8 &
+node ./lib/hand-writer.js RBF semeion-digits 10 &
+node ./lib/hand-writer.js MLP semeion-digits 1 20 &
+node ./lib/hand-writer.js MLP semeion-digits 1 40 &
+node ./lib/hand-writer.js MLP semeion-digits 1 60 &
+node ./lib/hand-writer.js MLP semeion-digits 1 80 &
+node ./lib/hand-writer.js MLP semeion-digits 1 100 &
+node ./lib/hand-writer.js MLP semeion-digits 2 20 &
+node ./lib/hand-writer.js MLP semeion-digits 2 40 &
+node ./lib/hand-writer.js MLP semeion-digits 2 60 &
+node ./lib/hand-writer.js MLP semeion-digits 2 80 &
+node ./lib/hand-writer.js MLP semeion-digits 2 100 &
+node ./lib/hand-writer.js ANFIS semeion-digits 2 &
+node ./lib/hand-writer.js ANFIS semeion-digits 3 &
+node ./lib/hand-writer.js ANFIS semeion-digits 4 &
+node ./lib/hand-writer.js ANFIS semeion-digits 5 &
+node ./lib/hand-writer.js ANFIS semeion-digits 6 
