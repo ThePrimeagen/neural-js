28e7ffdbd96637f04fa7f77ea72cd4569f44c724
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-23 13:46:35 -0600

    New converge.

--- scripts/rbf_all.sh
+++ scripts/rbf_all.sh
@@ -1,9 +1,4 @@
 cd ../
-node ./lib/hand-writer.js RBF letter-rec 2 &
-node ./lib/hand-writer.js RBF letter-rec 4 &
-node ./lib/hand-writer.js RBF letter-rec 6 &
-node ./lib/hand-writer.js RBF letter-rec 8 &
-node ./lib/hand-writer.js RBF letter-rec 10 
 node ./lib/hand-writer.js RBF opt-digits 2 &
 node ./lib/hand-writer.js RBF opt-digits 4 &
 node ./lib/hand-writer.js RBF opt-digits 6 &
