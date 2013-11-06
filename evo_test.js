0051160eabedd72b39daf06021db44a2790b8991
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-11-05 17:47:42 -0700

    Just crsuhing

---
+++ evo_test.js
@@ -0,0 +1,4 @@
+var evolution = require('ixjs-evolution');
+var RBFNetwork = require('./lib/neural-networks/RBFNetwork');
+var EvolutionaryRBFNetwork = require('./lib/neural-networks/EvolutionaryRBFNetwork');
+var testData = require('./data/')
