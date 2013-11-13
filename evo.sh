4338546ca3de4cc97784f47fb28895d2e6ac689c
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-11-12 22:19:19 -0700

    Latest code units

---
+++ evo.sh
@@ -0,0 +1,34 @@
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE &
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js energy-efficiency GA
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE &
+node ./evo_test.js mammograph DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
