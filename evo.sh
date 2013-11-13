a5cdb88e2505867e906df0fd79d2c65a05f7235f
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-11-13 14:56:44 -0700

    Some awesomeness.

--- evo.sh
+++ evo.sh
@@ -1,34 +1,240 @@
-node ./evo_test.js casp GA &
-node ./evo_test.js casp ES &
-node ./evo_test.js casp GA &
-node ./evo_test.js casp ES
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
 node ./evo_test.js ecoli GA &
 node ./evo_test.js ecoli ES &
 node ./evo_test.js ecoli DE
-node ./evo_test.js ecoli GA &
-node ./evo_test.js ecoli ES &
-node ./evo_test.js ecoli DE &
 node ./evo_test.js energy-efficiency GA &
 node ./evo_test.js energy-efficiency ES &
 node ./evo_test.js energy-efficiency DE &
-node ./evo_test.js energy-efficiency GA
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js energy-efficiency GA &
 node ./evo_test.js energy-efficiency ES &
 node ./evo_test.js energy-efficiency DE &
 node ./evo_test.js machine GA &
 node ./evo_test.js machine ES &
 node ./evo_test.js machine DE
-node ./evo_test.js machine GA &
-node ./evo_test.js machine ES &
-node ./evo_test.js machine DE &
 node ./evo_test.js mammograph DE &
-node ./evo_test.js seeds GA &
-node ./evo_test.js seeds ES &
-node ./evo_test.js seeds GA &
-node ./evo_test.js seeds ES
-node ./evo_test.js transfusion GA &
-node ./evo_test.js transfusion ES &
-node ./evo_test.js transfusion DE &
-node ./evo_test.js transfusion DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
 node ./evo_test.js yacht GA &
 node ./evo_test.js yacht ES &
 node ./evo_test.js yacht DE
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js banknote GA &
+node ./evo_test.js banknote ES &
+node ./evo_test.js banknote DE &
+node ./evo_test.js diabetes GA &
+node ./evo_test.js diabetes ES &
+node ./evo_test.js diabetes DE &
+node ./evo_test.js ecoli GA &
+node ./evo_test.js ecoli ES &
+node ./evo_test.js ecoli DE
+node ./evo_test.js energy-efficiency GA &
+node ./evo_test.js energy-efficiency ES &
+node ./evo_test.js energy-efficiency DE &
+node ./evo_test.js machine GA &
+node ./evo_test.js machine ES &
+node ./evo_test.js machine DE
+node ./evo_test.js mammograph DE &
+node ./evo_test.js mammograph ES &
+node ./evo_test.js mammograph GA &
+node ./evo_test.js yacht GA &
+node ./evo_test.js yacht ES &
+node ./evo_test.js yacht DE
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js transfusion GA &
+node ./evo_test.js transfusion ES &
+node ./evo_test.js transfusion DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE &
+node ./evo_test.js seeds GA &
+node ./evo_test.js seeds ES &
+node ./evo_test.js seeds DE
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE &
+node ./evo_test.js casp GA &
+node ./evo_test.js casp ES &
+node ./evo_test.js casp DE
